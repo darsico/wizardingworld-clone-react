c65d7dddb24e833e3f7fcc21862ea7e4753efd74
Gino Diez <diezsilvag@gmail.com>
2022-03-27 22:15:46 -0500

    Agregando components LastestNews y MagicalFeatures

---
+++ src/components/LastestNews.js
@@ -0,0 +1,42 @@
+const LastestNews = ({LastestNewsData}) => {
+
+    return (
+        <section className="lastest-news">
+            <div className="lastest-news__container">
+                <h2 className="lastest-news__title">Lastest News</h2>
+
+                <div className="lastest-news__news" id="lastestNewsNews">
+                    <div className="lastest-news__new">
+                        <figure className="lastest-news__image-container">
+                            <img src={LastestNewsData[0].src} alt="imagen de las news" className="lastest-news__image" />
+                        </figure>
+                        <p className="lastest-news__link-paragraph">
+                            <a href={LastestNewsData[0].href} className="lastest-news__paragraph">{LastestNewsData[0].paragraph}</a>
+                        </p>
+                    </div>
+
+                    <div className="lastest-news__new">
+                        <figure className="lastest-news__image-container">
+                            <img src={LastestNewsData[1].src} alt="imagen de las news" className="lastest-news__image" />
+                        </figure>
+                        <p className="lastest-news__link-paragraph">
+                            <a href={LastestNewsData[1].href} className="lastest-news__paragraph">{LastestNewsData[1].paragraph}</a>
+                        </p>
+                    </div>
+
+                    <div className="lastest-news__new">
+                        <figure className="lastest-news__image-container">
+                            <img src={LastestNewsData[2].src} alt="imagen de las news" className="lastest-news__image" />
+                        </figure>
+                        <p className="lastest-news__link-paragraph">
+                            <a href={LastestNewsData[2].href} className="lastest-news__paragraph">{LastestNewsData[2].paragraph}</a>
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default LastestNews;
---
+++ src/components/MagicalFeatures.js
@@ -0,0 +1,55 @@
+const MagicalFeatures = () => {
+    return(
+        <section className="magical-features">
+            <div className="magical-features__container">
+                <h2 className="magical-features__title">Magical features</h2>
+                <div className="magical-features__features" id="magicalFeaturesFeatures">
+                    
+                    <div className="magical-features__features-first" id="magicalFeaturesFeaturesFirst">
+                        <div className="magical-features__feature">
+                            <figure className="magical-features__image-container">
+                                <img src="https://images.ctfassets.net/usf1vwtuqyxm/7wO2s12BFbY8l1QMJPOyqQ/5355199bee35f1170f349ff66c2cac23/jude-law-dumbledore-fantastc-beasts-crimes-of-grindelwald.jpg?w=600&h=336&fit=fill&fm=webp&q=70" alt="imagen magical feature" className="magical-features__image" />
+                            </figure>
+                            <a href="https://www.wizardingworld.com/features/the-importance-of-dumbledores-many-armies" className="magical-features__link-text">
+                                <p className="magical-features__text">The importance of Dumbledore’s many armies</p>
+                            </a>
+                        </div>
+    
+                        <div className="magical-features__feature">
+                            <figure className="magical-features__image-container">
+                                <img src="https://images.ctfassets.net/usf1vwtuqyxm/1i6smoGEfXtPp5C07LaUjq/07d5b990cef870a234381ef97fbea0ef/WB-goblet-of-fire-harry-ron-hermione-and-quills.jpg?w=600&h=336&fit=fill&fm=webp&q=70" alt="imagen magical feature" className="magical-features__image" />
+                            </figure>
+                            <a href="https://www.wizardingworld.com/features/wizarding-world-cryptic-crossword" className="magical-features__link-text">
+                                <p className="magical-features__text">Play our Wizarding World cryptic crossword</p>
+                            </a>
+                        </div>
+
+                    </div>
+                    
+                    <div className="magical-features__features-second" id="magicalFeaturesFeaturesSecond">
+                        <div className="magical-features__feature">
+                            <figure className="magical-features__image-container">
+                                <img src="https://images.ctfassets.net/usf1vwtuqyxm/22tUM8mtX5MrVweDYYtE8X/6b6edf5c0d3796c5b7bacbb247e3fc7e/Howler_3.jpg?w=600&h=336&fit=fill&fm=webp&q=70" alt="imagen magical feature" className="magical-features__image" />
+                            </figure>
+                            <a href="https://www.wizardingworld.com/features/watch-our-video-tutorial-on-how-to-make-a-paper-craft-howler" className="magical-features__link-text">
+                                <p className="magical-features__text">Watch our video tutorial on how to make a paper craft Howler</p>
+                            </a>
+                        </div>
+    
+                        <div className="magical-features__feature">
+                            <figure className="magical-features__image-container">
+                                <img src="https://images.ctfassets.net/usf1vwtuqyxm/5gsLEZsT5iFCtliSP88Uhi/9684cee57cbd12326174bf9b696cb2ca/telephone-box-harry.jpg?w=600&h=336&fit=fill&fm=webp&q=70" alt="imagen magical feature" className="magical-features__image" />
+                            </figure>
+                            <a href="https://www.wizardingworld.com/features/web-hidden-entrances-wizarding-world" className="magical-features__link-text">
+                                <p className="magical-features__text">Hidden entrances of the wizarding world</p>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default MagicalFeatures;
